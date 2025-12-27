db14505073e5b6e7c25bd03f2709b4e23e6aa53f
Ruihuang Zhang <zrh10077707@hotmail.com>
2025-12-27 20:10:19 +0800

    完成了比赛：T1、T2:100 T3:0

--- T1.cpp
+++ T1.cpp
@@ -1,41 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m;
-struct edge{
-	int x,y,l;
-}a[200010];
-int p[200010],sum;
+int p[200010];
 int find(int x){
-	if(p[x]=x) return x;
-	return p[x]=find(p[x]);
+    if(p[x]!=x) p[x]=find(p[x]);
+    return p[x];
 }
 void unite(int x,int y){
-	int f1=find(x),f2=find(y);
-	if(f2!=f1) p[f2]=f1;
-	return;
+    int f1=find(x),f2=find(y);
+    if(f1!=f2) p[f1]=f2;
 }
+struct edge{
+    int u,v,l;
+}a[200010];
 bool cmp(edge x,edge y){
-	return x.l<y.l;
+    return x.l<y.l;
 }
+int n,m,k;
 int main(){
-	cin>>n>>m;
-	for(int i=1;i<=m;i++){
-		cin>>a[i].x>>a[i].y>>a[i].l;
-		sum+=a[i].l;
-	}
-	sort(a+1,a+1+n,cmp);
-	for(int i=1;i<=n;i++) p[i]=i;
-	int ans=0,cnt=0;
-	for(int i=1;i<=n;i++){
-		int f1=find(a[i].x),f2=find(a[i].y);
-		if(f1!=f2){
-			unite(f1,f2);
-			ans+=a[i].l;
-			cnt++;
-			if(cnt==n-1) break;
-		}
-	}
-	if(cnt==n-1) cout<<ans;
-	else cout<<"orz";
-	return 0;
-} 
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    cin>>n>>m;
+    k=1;
+    for(int i=1;i<=n;i++) p[i]=i;
+    for(int i=1;i<=m;i++){
+        int x,y,l;
+        cin>>x>>y>>l;
+        a[i]={x,y,l};
+    }
+    int cnt=0,ans=0;
+    sort(a+1,a+m+1,cmp);
+    if(k==n){
+        cout<<0<<"\n";
+        return 0;
+    }
+    for(int i=1;i<=m;i++){
+        if(cnt==n-1) break;
+        int f1=find(a[i].u),f2=find(a[i].v);
+        if(f1!=f2){
+            unite(a[i].u,a[i].v);
+            ans+=a[i].l;
+            cnt++;
+        }
+    }
+    if(cnt==n-k){
+        cout<<ans<<"\n";
+        return 0;
+    }
+    else{
+        cout<<"orz"<<"\n";
+        return 0;
+    }
+    return 0;
+}
